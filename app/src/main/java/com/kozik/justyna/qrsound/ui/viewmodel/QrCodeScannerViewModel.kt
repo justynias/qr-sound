baf2d893b362a6525e3d12782458ef06dc41eb52
justynakozik <azh92@eur.corp.vattenfall.com>
2020-09-27 00:39:40 +0200

    very basic ui with initial feature of scanning and playing the sounds

--- app/src/main/java/com/kozik/justyna/qrsound/ui/viewmodel/QrCodeScannerViewModel.kt
+++ app/src/main/java/com/kozik/justyna/qrsound/ui/viewmodel/QrCodeScannerViewModel.kt
@@ -1,31 +1,37 @@
 package com.kozik.justyna.qrsound.ui.viewmodel
 
+import android.media.MediaPlayer
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.kozik.justyna.qrsound.BuildConfig
 import com.kozik.justyna.qrsound.services.ApiClient
 import com.kozik.justyna.qrsound.services.data.response.SoundResponse
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import retrofit2.Response
-
-//import io.reactivex.rxkotlin.subscribeBy
+import java.util.*
 
 class QrCodeScannerViewModel : ViewModel() {
-    val hash: MutableLiveData<String> = MutableLiveData("229e9253-5fe4-49b0-b0b4-29046fca45c8")
 
+    private val service = ApiClient().service
+    private val mediaPlayer = MediaPlayer()
+    val hash: MutableLiveData<String> = MutableLiveData()
     val description = MutableLiveData<String>()
-    //fun description(): LiveData<String> = description
-
-    //"229e9253-5fe4-49b0-b0b4-29046fca45c8"
+    val isSoundPlayed = MutableLiveData<Boolean>(false)
 
     fun updateSoundDescription() {
-        val service = ApiClient().service
         service.getSound(hash.value.toString())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
             .subscribe({ it -> onResponse(it) }, { it -> onFailure(it) })
 
+    }
+
+    fun onScanAgainClicked() {
+        onStopSoundClicked()
+        hash.value = null
+        description.value = null
     }
 
     private fun onFailure(t: Throwable) {
@@ -39,4 +45,49 @@ class QrCodeScannerViewModel : ViewModel() {
             description.value = soundResponse.code().toString()
         }
     }
+//
+//    fun onStartStopSoundClicked() {
+//        if (mediaPlayer.isPlaying) {
+//            stopPlayingSound()
+//        }  else {
+//
+//            startPlayingSound()
+//        }
+//    }
+
+    fun onStopSoundClicked() {
+        mediaPlayer.reset()
+    }
+
+    fun onStartSoundClicked() {
+        val url = "${BuildConfig.API_KEY}public/${hash.value}"
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepare()
+
+        val duration = mediaPlayer.duration.toLong()
+        val amoungToupdate = duration / 100.toLong()
+
+
+        mediaPlayer.start()
+        isSoundPlayed.value = true
+        val timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                // runOnUiThread {
+                Log.d("PLAYER", "Current position: ${mediaPlayer.currentPosition}")
+                Log.d("PLAYER", "amoungToupdate: ${amoungToupdate}")
+                //}
+            }
+        }, 0, amoungToupdate)
+
+        mediaPlayer.setOnCompletionListener {
+            timer.cancel()
+            mediaPlayer.reset()
+            isSoundPlayed.value = false
+        }
+
+
+    }
+
+
 }
